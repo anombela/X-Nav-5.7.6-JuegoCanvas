8b23f4c4b20ab34884b8f95fefbc51e8102036cd
anombela <a.nombela93@hotmail.com>
2016-02-25 23:54:34 +0100

    hechas varias cosas

--- js/game.js
+++ js/game.js
@@ -3,6 +3,11 @@
 // Slight modifications by Gregorio Robles <grex@gsyc.urjc.es>
 // to meet the criteria of a canvas class for DAT @ Univ. Rey Juan Carlos
 
+
+//princesa ocupa 16
+//arboles ocupan 32
+
+
 // Create the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
@@ -34,15 +39,48 @@ princessImage.onload = function () {
 };
 princessImage.src = "images/princess.png";
 
+//piedras.......................................
+var stoneReady = false;
+var stoneImage = new Image();
+stoneImage.onload = function () {
+	stoneReady = true;
+};
+stoneImage.src = "images/stone.png";
+//...............................................
+
+//monstruos.......................................
+var monsterReady = false;
+var monsterImage = new Image();
+monsterImage.onload = function () {
+	monsterReady = true;
+};
+monsterImage.src = "images/monster.png";
+//...............................................
+
+
+
 // Game objects
 var hero = {
-	speed: 256 // movement in pixels per second
+	speed: 256, // movement in pixels per second
+	x: canvas.width / 2,
+	y: canvas.height / 2
 };
 var princess = {};
-var princessesCaught = 0;
+var princessesCaught = 0;  //princesas atrapadas
+
+var stones = [  //array de piedras
+];
+var numstones = 3;
+
+var monsters = [  //array de monstruos
+];
+var nummonsters = 1;
+
 
 // Handle keyboard controls
 var keysDown = {};
+var pause = false;
+console.log(pause)
 
 addEventListener("keydown", function (e) {
 	keysDown[e.keyCode] = true;
@@ -52,63 +90,181 @@ addEventListener("keyup", function (e) {
 	delete keysDown[e.keyCode];
 }, false);
 
+addEventListener("keypress", function(e){
+	if (e.charCode == 32){
+		var p = document.getElementById("pause");
+		var texto = "";
+		if (pause){
+			pause = false;
+		}else{
+			texto = "<h2>PAUSA</h2>"
+			pause = true;
+		}
+		p.innerHTML = texto;
+	}
+}, false);
+
+
+
+  
+function stonetouching(){
+
+	for( var i = 0 ;  i < stones.length ; i++){
+		if (hero.x <= (stones[i].x + 16)  
+			&& stones[i].x <= (hero.x + 16) 
+			&& hero.y <= (stones[i].y + 16) 
+			&& stones[i].y <= (hero.y + 16))
+		
+			return true;  //si alguno se da acaba como true
+	}
+	return false;
+	
+
+};
+
 // Reset the game when the player catches a princess
-var reset = function () {
-	hero.x = canvas.width / 2;
-	hero.y = canvas.height / 2;
+var reset = function () {//posicion inicial de princesa y heroe
+	/*hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;*/
+	stones = [];
+	monsters = [];
 
 	// Throw the princess somewhere on the screen randomly
-	princess.x = 32 + (Math.random() * (canvas.width - 64));
-	princess.y = 32 + (Math.random() * (canvas.height - 64));
+	//-----con los bucles la princesa ya no estara entre los arboles nunca
+	do{
+		princess.x = 32 + (Math.random() * (canvas.width - 64));
+	}while(princess.x > canvas.width - 64);
+	do{
+		princess.y = 32 + (Math.random() * (canvas.height - 64));
+	}while(princess.y > canvas.height - 64);
+
+
+	for( var i = 0 ;  i < numstones; i++){
+		var stone = {};
+		do{
+			stone.x = 32 + (Math.random() * (canvas.width - 64));
+		}while(stone.x > canvas.width - 64);
+		do{
+			stone.y = 32 + (Math.random() * (canvas.height - 64));
+		}while(stone.y > canvas.height - 64);
+
+		stones.push(stone);
+	}
+
+	for( var i = 0 ;  i < nummonsters; i++){
+		var monster = {};
+		do{
+			monster.x = 32 + (Math.random() * (canvas.width - 64));
+		}while(monster.x > canvas.width - 64);
+		do{
+			monster.y = 32 + (Math.random() * (canvas.height - 64));
+		}while(stone.y > monster.height - 64);
+
+		monsters.push(monster);
+	}
+
 };
 
 // Update game objects
-var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
+var update = function (modifier) { //modifier es variable de tiempo creo
+
+	if (pause) { // nada se mueve en pausa
+		return;	
+	}
+	
+
+	if (38 in keysDown ) { // Player holding up
+		var aux = hero.y;
 		hero.y -= hero.speed * modifier;
+		if (hero.y <= 32 || stonetouching())
+			hero.y = aux;
 	}
 	if (40 in keysDown) { // Player holding down
+		var aux = hero.y;
 		hero.y += hero.speed * modifier;
+		if (hero.y >= canvas.height - 64 || stonetouching())
+			hero.y = aux;
 	}
 	if (37 in keysDown) { // Player holding left
+		var aux = hero.x;
 		hero.x -= hero.speed * modifier;
+		if (hero.x < 32 || stonetouching())
+			hero.x = aux;
 	}
 	if (39 in keysDown) { // Player holding right
+		var aux = hero.x;
 		hero.x += hero.speed * modifier;
+		if (hero.x >= canvas.width - 64 || stonetouching())
+			hero.x = aux;
 	}
 
-	// Are they touching?
+//////////mosterssss movimientos///////////
+	for( var i = 0 ;  i < nummonsters; i++){
+		if (monsters[i].x < hero.x){
+			monsters[i].x += (hero.speed/8) * modifier
+		}else{
+			monsters[i].x -= (hero.speed/8) * modifier
+		}
+
+		if (monsters[i].y < hero.y){
+			monsters[i].y += (hero.speed/8) * modifier
+		}else{
+			monsters[i].y -= (hero.speed/8) * modifier
+		}
+	}
+///////////////////////////////////////////
+
+
+
+
+	// Are they touching?  //se tocan la princesa y el heroe
 	if (
-		hero.x <= (princess.x + 16)
-		&& princess.x <= (hero.x + 16)
-		&& hero.y <= (princess.y + 16)
-		&& princess.y <= (hero.y + 32)
+		hero.x <= (princess.x + 16)  //toca por el lado derecho de la princesa
+		&& princess.x <= (hero.x + 16)  //lado izquierdo princesa
+		&& hero.y <= (princess.y + 16)  //abajo princesa
+		&& princess.y <= (hero.y + 16)   //arriba princesa
 	) {
 		++princessesCaught;
-		reset();
+		 numstones = 3 + Math.floor(princessesCaught / 3 ) //cada 5 princesas 1 piedra mas
+		reset();  //para resetear(seguir jugando)
 	}
 };
 
 // Draw everything
 var render = function () {
-	if (bgReady) {
+	if (bgReady) { ///si esta ya cargado el background
 		ctx.drawImage(bgImage, 0, 0);
 	}
 
-	if (heroReady) {
+	if (heroReady) { //ha cargado el heroe
 		ctx.drawImage(heroImage, hero.x, hero.y);
 	}
 
-	if (princessReady) {
+	if (princessReady) {  //ha cargado la princesa
 		ctx.drawImage(princessImage, princess.x, princess.y);
 	}
+	//++++++++++++++++++++++++++++++++
+	if (stoneReady) {  //ha cargado la princesa
+		for( var i = 0 ;  i < stones.length ; i++){
+			ctx.drawImage(stoneImage, stones[i].x, stones[i].y);
+		}
+	}
+	//++++++++++++++++++++++++++++
+	//++++++++++++++++++++++++++++++++
+	if (monsterReady) {  //ha cargado la princesa
+		for( var i = 0 ;  i < monsters.length ; i++){
+			ctx.drawImage(monsterImage, monsters[i].x, monsters[i].y);
+		}
+	}
+	//++++++++++++++++++++++++++++
+	//hasta aqui ha pintada el fondo la princesa y el heroe
 
-	// Score
+	// Score  //el texto que aparece
 	ctx.fillStyle = "rgb(250, 250, 250)";
 	ctx.font = "24px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
-	ctx.fillText("Princesses caught: " + princessesCaught, 32, 32);
+	ctx.fillText("Princesses caught: " + princessesCaught, 32, 32); //escribe texto
 };
 
 // The main game loop
@@ -127,4 +283,4 @@ reset();
 var then = Date.now();
 //The setInterval() method will wait a specified number of milliseconds, and then execute a specified function, and it will continue to execute the function, once at every given time-interval.
 //Syntax: setInterval("javascript function",milliseconds);
-setInterval(main, 1); // Execute as fast as possible
+setInterval(main, 1); // Execute as fast as possible  //bucle que se ejecuta cada milisegundo
